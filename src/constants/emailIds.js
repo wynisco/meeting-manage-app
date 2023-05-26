2d45d76f897dfb028ef304d0ac90ce782cbb6233
Harsh Gupta <harshgupta@Harshs-MacBook-Air-2.local>
2023-05-26 11:16:51 +0530

    stage

--- src/constants/emailIds.js
+++ src/constants/emailIds.js
@@ -14,6 +14,9 @@ const EMAIL_IDS = [
   {
     email: "react@wynisco.com",
   },
+  {
+    email: "harshgupta.harry@gmail.com",
+  },
 ];
 
 const EMAIL_ID_STRING_ARRAY = [
@@ -22,6 +25,7 @@ const EMAIL_ID_STRING_ARRAY = [
   "wyniscouser1@gmail.com",
   "sachin@wynisco.com",
   "react@wynisco.com",
+  "harshgupta.harry@gmail.com",
 ];
 export default EMAIL_IDS;
 export { EMAIL_ID_STRING_ARRAY };
