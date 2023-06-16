ff500c8854b61dae70208414641547906522fc2b
saipraneeth <saipraneethcheekati@gmail.com>
2023-06-16 18:07:13 -0500

    Pop Up code has been added

--- src/pages/calender/meetings.js
+++ src/pages/calender/meetings.js
@@ -1,24 +1,25 @@
 import React from "react";
+import "./meeting.css";
 
 const Meetings = ({ meetings = [] }) => {
   return (
     <div style={{ paddingBottom: 30 }}>
       {meetings?.map((meet, i) => {
         return (
-          <div
-            style={{
-              background: "#0070f3",
-              color: "white",
-              borderBottom: "2px solid white",
-              justifyContent: "space-between",
-              flexDirection: "row",
-              display: "flex",
-              paddingRight: 10,
-              paddingLeft: 10,
-            }}
-          >
-            <div>{meet?.topic}</div>
-            <div style={{ width: 70, textAlign: "right" }}>{meet?.duration} mins</div>
+          <div>
+            <div className="meetings_cell">
+              <div className="meetings_cell_inside">
+                <div>{meet?.topic}</div>
+                <div style={{ width: 70, textAlign: "right" }}>
+                  {`${meet?.duration} mins`}
+                </div>
+              </div>
+              <span className="hide_details">
+                <span>{meet?.topic}</span>
+                <br></br>
+                <span>{`Time: ${meet?.duration} minutes`}</span>
+              </span>
+            </div>
           </div>
         );
       })}
